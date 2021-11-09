bf64d7d5409f8a0492fe2df665a78fc40c10d0f3
TezBaydu <tezbaydu@gmail.com>
2021-11-09 21:21:23 +0000

    added projects app, models and fixtures

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Project(models.Model):
+    company = models.ForeignKey('Company', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
