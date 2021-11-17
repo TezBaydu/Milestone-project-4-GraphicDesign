dbad5a99497f62aed55e33c8d5a215fc72279a9f
TezBaydu <tezbaydu@gmail.com>
2021-11-17 21:59:39 +0000

    Added package template

--- kingsland_design/urls.py
+++ kingsland_design/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('projects/', include('projects.urls')),
+    path('packages/', include('packages.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- packages/admin.py
+++ packages/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Package
 
 
-admin.site.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'friendly_name',
+        'price',
+        'image',
+        'logo_count_request',
+        'quality_request',
+        'support_request',
+        'production_days',
+    )
+
+    ordering = ('sku',)
+
+admin.site.register(Package, PackageAdmin)
