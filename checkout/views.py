6331b23906d09f0f3acb50de8b792a121692db82
TezBaydu <tezbaydu@gmail.com>
2021-12-13 21:52:37 +0000

    Update to original gitpod file

--- checkout/views.py
+++ checkout/views.py
@@ -7,12 +7,12 @@ from .models import Order, OrderLineItem
 from packages.models import CompanyDetails
 from bag.contexts import bag_contents
 
-import stripe
+# import stripe
 
 
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    # stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    # stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     if request.method == 'POST':
         bag = request.session.get('bag', {})
@@ -48,7 +48,7 @@ def checkout(request):
                 except Package.DoesNotExist:
                     messages.error(request, (
                         "Package wasn't found in our database."
-                        "Please call for ssistance!")
+                        "Please call for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
@@ -77,27 +77,27 @@ def checkout(request):
             messages.error(request, "You currently have no orders")
             return redirect(reverse('packages'))
 
-        current_bag = bag_contents(request)
-        total = current_bag['grand_total']
-        stripe_total = round(total * 100)
-        stripe.api_key = stripe_secret_key
-        intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
+        # current_bag = bag_contents(request)
+        # total = current_bag['grand_total']
+        # stripe_total = round(total * 100)
+        # stripe.api_key = stripe_secret_key
+        # intent = stripe.PaymentIntent.create(
+        #     amount=stripe_total,
+        #     currency=settings.STRIPE_CURRENCY,
+        # )
 
-        order_form = OrderForm()
+        # order_form = OrderForm()
 
-    if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+    # if not stripe_public_key:
+    #     messages.warning(request, 'Stripe public key is missing. \
+    #         Did you forget to set it in your environment?')
 
-    template = 'checkout/checkout.html'
-    context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
-    }
+    # template = 'checkout/checkout.html'
+    # context = {
+    #     'order_form': order_form,
+    #     'stripe_public_key': stripe_public_key,
+    #     'client_secret': intent.client_secret,
+    # }
 
     return render(request, template, context)
 
