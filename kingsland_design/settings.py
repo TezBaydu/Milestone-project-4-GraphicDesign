9be0aa51a69c2c6855fc181f34e3005b5a4387f1
TezBaydu <tezbaydu@gmail.com>
2022-01-15 18:56:57 +0000

    webhook check

--- kingsland_design/settings.py
+++ kingsland_design/settings.py
@@ -179,6 +179,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe
 STRIPE_CURRENCY = 'gbp'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_KEY = os.getenv('STRIPE_WH_KEY', '')
+STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_KEY = os.environ.get('STRIPE_WH_KEY', '')
