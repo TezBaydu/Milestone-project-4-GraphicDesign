546e67f596df565b6f817355e145badcac003f75
TezBaydu <tezbaydu@gmail.com>
2022-01-20 19:14:56 +0000

    settings updated

--- kingsland_design/settings.py
+++ kingsland_design/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
-DEBUG = 'DEVELOPMENT' in os.eviron
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kingsland-design.herokuapp.com', 'localhost']
 
